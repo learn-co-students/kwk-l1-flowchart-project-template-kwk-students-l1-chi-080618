2995f289a6ce6e0e00c72c8ab7e1b3c8ad45b417
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-15 20:56:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,11 +24,39 @@ class ApplicationController < Sinatra::Base
     elsif city == "Milwaukee"
       return erb :milwaukee
     
-    elsif city == "St. Louis"
+    elsif city == "St._Louis"
       return erb :stlouis
     else
-      return "wrong page"
+      return "wrong_page"
     end 
   end
+  
+  
+  post '/milwaukee_quiz' do
+    activity = params[:activity]
+    if activity == "Workout"
+      return erb :milwaukee_workout
+      
+    elsif activity == "Eat_Breakfast"
+      return erb :milwaukee_breakfast
+      
+    elsif activity == "Go_Shopping"
+      return erb :milwaukee_shopping
+      
+    elsif activity == "Eat_Lunch"
+      return erb :milwaukee_lunch
+    
+    elsif activity == "Visit_a_Local_Landmark"
+      return erb :milwaukee_Landmark
+    
+    elsif activity == "Find_Entertainment"
+      return erb :milwaukee_entertainment
+      
+    elsif activity == "Eat_Dinner_and_Dessert"
+      return erb :milwaukee_dessert
+    end 
+  end 
+      
+      
 
 end
