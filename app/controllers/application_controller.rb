b1f85776ac0d44e4791aca79d7504311654bb09f
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-15 16:35:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,45 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  get '/'do
+    erb :index
+  end
+  
+  post '/result' do
+    
+    city = params[:city]
+    
+    if city == "Chicago"
+      return erb :chicago
+    else
+      return "wrong page"
+    end
+    
+    
+     if city == "Chicago"
+      return erb :chicago
+    else
+      return "wrong page"
+    end
+    
+     if city == "Detroit"
+      return erb :detroit
+    else
+      return "wrong page"
+    end
+    
+      if city == "Milwaukee"
+      return erb :milwaukee
+    else
+      return "wrong page"
+    end
+    
+     if city == "St. Louis"
+      return erb :stlouis
+    else
+      return "wrong page"
+    end
+
+
 
 end
