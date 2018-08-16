db0bcb73ac7fc20ca40ed884ea5a8e22eba50b71
lauren-hering <lauren.hering@comcast.net>
2018-08-16 21:21:56 +0000

    Done.

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,40 @@
-class Result
+def calculate_result(total)
+  if total <= 7
+    return "mars"
+  
+  elsif total <= 21
+    return "mercury"
+    
+  elsif total <= 28
+    return "moon"
+    
+  elsif total <= 35 
+    return "sun"
+    
+  elsif total <= 42
+    puts "pluto"
+    
+  elsif total <= 49
+    return "venus"
+    
+  elsif total <= 56
+    return "earth"
+    
+  elsif total <= 63
+    return "jupiter"
+    
+  elsif total <= 70
+    return "saturn"
+    
+  elsif total <= 77 
+    return "uranus"
+    
+  elsif total <= 84
+    return "neptune"
+    
+  else
+    return "alien"
+    
+  end 
 
 end
