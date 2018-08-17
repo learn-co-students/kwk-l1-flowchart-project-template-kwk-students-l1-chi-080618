fc82648c26d357057496034662725a577a0d708a
frisaac <frisaac@stu.naperville203.org>
2018-08-17 16:27:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,28 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   
   get '/' do
     erb :index
   end
+  
   get '/homepage' do
     erb :index
   end
+  
   get '/quiz' do
     erb :quiz
   end
+  
   get '/about_us' do
     erb :about_us
   end
+  
   get '/self_def_tips' do
     erb :self_def_tips
   end
+  
   post '/results' do
     
     place = params[:place]
@@ -29,37 +35,37 @@ class ApplicationController < Sinatra::Base
     commitment = params[:commitment]
     city = params[:city]
 
-    decision = []
+    @decision = []
     if place == "virtual"
       if price == "low"
         if commitment == "short"
           # if city == Chicago
-          decision << "a udemy web page instructing teens street safety"
-          decision << "https://www.udemy.com/street-safety-for-parents-of-teens-young-adults/"
+          @decision << "an udemy web page instructing teens on street safety."
+          @decision << "https://www.udemy.com/street-safety-for-parents-of-teens-young-adults/"
         else commitment == "long"
           # if city == Chicago
-          decision << "a youTube channel meant to instruct on self defense"
-          decision << "https://www.youtube.com/channel/UCj4JOZ3jvgkHPYew0ksfDZw"
+          @decision << "a youTube channel meant to instruct on self defense."
+          @decision << "https://www.youtube.com/channel/UCj4JOZ3jvgkHPYew0ksfDZw"
         end
       elsif price == "med"
         if commitment == "short"
           # if city == Chicago
-          decision << "one hour video course from ProTrainings"
-          decision << "https://www.protrainings.com/en/courses/210-self-defense?topic_id=8-self-defense"
+          @decision << "a one hour video course from ProTrainings."
+          @decision << "https://www.protrainings.com/en/courses/210-self-defense?topic_id=8-self-defense"
         else commitment == "long"
           # if city == Chicago
-          decision << "searching the S2 Academy courses."
-          decision << "http://www.s2online.academy/courses/fighting-chance-womens-self-defense-course"
+          @decision << "searching the S2 Academy courses."
+          @decision << "http://www.s2online.academy/courses/fighting-chance-womens-self-defense-course"
         end
       else price == "high"
         if commitment == "short"
           # if city == Chicago
-          decision << "searching for one on your own"
-          decision << "we are unable to find one with that criterion"
+          @decision << "searching for one on your own"
+          @decision << "we are unable to find one with that criterion"
         else commitment == "long"
           # if city == Chicago
-          decision << "buying a self defense study kit from the Self Defense College."
-          decision << "https://www.selfdefensecollege.com/"
+          @decision << "buying a self defense study kit from the Self Defense College."
+          @decision << "https://www.selfdefensecollege.com/"
         end
       end
       
@@ -67,42 +73,43 @@ class ApplicationController < Sinatra::Base
       if price == "low"
         if commitment == "short"
           # if city == Chicago
-          decision << "attending a session offered free from the University of Illinois at Chicago police."
-          decision << "https://today.uic.edu/uic-police-offer-womens-self-defense-courses"
+          @decision << "attending a session offered free from the University of Illinois at Chicago police."
+          @decision << "https://today.uic.edu/uic-police-offer-womens-self-defense-courses"
         else commitment == "long"
           # if city == Chicago
-          decision << "buying a membership to the IL Shotokan Karate Clubs."
-          decision << "https://www.iskc.com/"
+          @decision << "buying a membership to the IL Shotokan Karate Clubs."
+          @decision << "https://www.iskc.com/"
         end
       elsif price == "med"
         if commitment == "short"
           # if city == Chicago
-          decision << "investing in a personal trainer."
-          decision << "http://www.orangeshoe.com/Lakeview/PersonalTrainerCosts "
+          @decision << "investing in a personal trainer."
+          @decision << "http://www.orangeshoe.com/Lakeview/PersonalTrainerCosts "
         else commitment == "long"
           # if city == Chicago
-          decision << "investing in a membership to 9round."
-          decision << "https://www.9round.com/"
+          @decision << "investing in a membership to 9round."
+          @decision << "https://www.9round.com/"
         end
       elsif price == "high"
         if commitment == "short"
           # if city == Chicago
-          decision << "enrolling in an Impact Chicago workshop."
-          decision << "http://www.impactchicago.org/"
+          @decision << "enrolling in an Impact Chicago workshop."
+          @decision << "http://www.impactchicago.org/"
         else commitment == "long"
           # if city == Chicago
-          decision << "becoming a member of the Chicago MMA branch."
-          decision << "http://www.chicagomma.com/?gclid=CjwKCAjwns_bBRBCEiwA7AVGHmC1TKLENP4Qhsaq72LVqYDhlg-tedpj5uZNZuhfatuzC2cUUyyEHRoCctsQAvD_BwE#mixed-martial-arts"
+          @decision << "becoming a member of the Chicago MMA branch."
+          @decision << "http://www.chicagomma.com/?gclid=CjwKCAjwns_bBRBCEiwA7AVGHmC1TKLENP4Qhsaq72LVqYDhlg-tedpj5uZNZuhfatuzC2cUUyyEHRoCctsQAvD_BwE#mixed-martial-arts"
         end
       end
-
-
-    else
-      return "wrong page"
     end
-    return "Due to your selections, we would recommend #{decision[0]} (#{decision[1].gsub(URI.regexp, '<a href="\0">\0</a>')
-})."
+
+    # return "Due to your selections, we would recommend #{@decision[0]} (#{@decision[1].gsub(URI.regexp, '<a href="\0">\0</a>')})."
     
+    # @str1 = decision[0]
+    # # @str2 = decision[1].gsub(URI.regexp, '<a href="\0">\0</a>')
+    # @str2 = decision[1]
+    
+    return erb :decision
     
   end
   # get '/about_us' do
